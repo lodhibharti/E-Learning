d93f3ac06904fc895e75476bff8b1687519c1ed6
uma <info@bhartilodhi.com>
2020-11-15 17:12:37 -0800

    initial commit

--- index.php
+++ index.php
@@ -3,37 +3,39 @@
 <title>E-Learning Site</title>
 
 <!-- Link to local CSS file -->
-<link rel="stylesheet" href="assets/Docs/CSS/main.css">
+<link rel="stylesheet" href="assets/Docs/CSS/Main.css">
 <meta charset="utf-8">
   <meta name="viewport"content="width=device-width, initial-scale=1">
   <link rel="stylesheet"href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
-<style>
-</style>
+
 </head>
 <body>
 
 <!-- Website logo -->
 <div class="navbar-header">
-                <a href="index.php" class="navbar-brand">E-Learning Site</a>         
-            </div>
+    <a href="home.php" class="navbar-brand" style="text-align:center; padding: 10px 0; background-color:#000000; color:white; bottom: 1%; width: 100%;">Visit All Notes</a>         
+</div>
 
 <!-- Main Content -->     
 <div>
-<img src="assets\images\image-20150622-17748-1629t5g.jpg" height="100%" width="100%">
+<img src="assets\images\image-20150622-17748-1629t5g.jpg" height="90%" width="100%" alt="Notes">
 </div>
-
+  <div>
+      <h4 style="text-align:center; margin-top:1%"><b>Hello, Welcome To E-Learning</b></h4>
+      <p style="text-align:center;">Everyone can access their learning materials online at anywhere and at any time.</p>
+</div>
 
 <!-- Footer-->
 <footer>
-        <div class="container home-center-content">
-            <p class="lead">
+        <div>
+            <p>
                 Copyright © E-Learning Site | All Rights Reserved | Contact Us: +91 8827863754
             </p>
         </div>
-    </footer>
+</footer>
 <!--/Footer-->
 
 </body>
